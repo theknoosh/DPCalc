258c05c01c7ba798e4453c53861d86fdf6f6fbbb
Darrell Payne <d.anthony.payne@gmail.com>
2017-07-13 22:23:29 -0700

    Interface elements drawn and connected

--- DPCalc WatchKit Extension/InterfaceController.swift
+++ DPCalc WatchKit Extension/InterfaceController.swift
@@ -11,7 +11,42 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    
+    @IBOutlet var label: WKInterfaceLabel!
+    
+    @IBAction func tapped0() {tappedNumber(num: 0)}
+    @IBAction func tapped1() {tappedNumber(num: 1)}
+    @IBAction func tapped2() {tappedNumber(num: 2)}
+    @IBAction func tapped3() {tappedNumber(num: 3)}
+    @IBAction func tapped4() {tappedNumber(num: 4)}
+    @IBAction func tapped5() {tappedNumber(num: 5)}
+    @IBAction func tapped6() {tappedNumber(num: 6)}
+    @IBAction func tapped7() {tappedNumber(num: 7)}
+    @IBAction func tapped8() {tappedNumber(num: 8)}
+    @IBAction func tapped9() {tappedNumber(num: 9)}
 
+    
+    func tappedNumber(num:Int){
+        
+    }
+
+    @IBAction func tappedPlus(){
+        
+    }
+    
+    @IBAction func tappedMinus(){
+        
+    }
+
+    @IBAction func tappedClear(){
+        
+    }
+
+    @IBAction func tappedEquals(){
+        
+    }
+
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
